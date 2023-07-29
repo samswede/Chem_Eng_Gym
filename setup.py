787f80afb98dc8346ad76d1c8272e38fb02a1cb2
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-07-29 23:50:16 +0200

    added email

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='Chem_Eng_Gym',
     version='0.1.0',
-    url='https://github.com/yourusername/Chem_Eng_Gym',
+    url='https://github.com/samswede/Chem_Eng_Gym.git',
     author='Samuel Martin Andersson',
-    author_email='youremail@gmail.com',
+    author_email='samuel.andersson19@imperial.ac.uk',
     description='Description of your package',
     packages=find_packages(where="src"),  # automatically find all packages and subpackages in 'src' directory
     package_dir={"": "src"},  # tell setuptools that packages are under 'src' directory
